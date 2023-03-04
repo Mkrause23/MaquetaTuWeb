4d16fa036536937bbe404244256e5e00c7d152fc
Manuel krause <96996916+Mkrause23@users.noreply.github.com>
2023-03-04 00:46:32 -0300

    primeros archivos en edición

---
+++ assets/scripts.js
@@ -0,0 +1,282 @@
+content = document.querySelector(".content");
+content.onclick = function() {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+var boton1 = document.createElement('button');
+boton1.innerHTML = 'Hello :D';
+boton1.className = 'boton1 button';
+document.querySelector('.div').appendChild(boton1);
+var boton2 = document.createElement('button');
+boton2.innerHTML = 'Hello :D';
+boton2.className = 'boton2 button';
+document.querySelector('.div').appendChild(boton2);
+var boton3 = document.createElement('button');
+boton3.innerHTML = 'Hello :D';
+boton3.className = 'boton3 button';
+document.querySelector('.div').appendChild(boton3);
+var boton4 = document.createElement('button');
+boton4.innerHTML = 'Hello :D';
+boton4.className = 'boton4 button';
+document.querySelector('.div').appendChild(boton4);
+var boton5 = document.createElement('button');
+boton5.innerHTML = 'Hello :D';
+boton5.className = 'boton5 button';
+document.querySelector('.div').appendChild(boton5);
+var boton6 = document.createElement('button');
+boton6.innerHTML = 'Hello :D';
+boton6.className = 'boton6 button';
+document.querySelector('.div').appendChild(boton6);
+var boton7 = document.createElement('button');
+boton7.innerHTML = 'Hello :D';
+boton7.className = 'boton7 button';
+document.querySelector('.div').appendChild(boton7);
+var boton8 = document.createElement('button');
+boton8.innerHTML = 'Hello :D';
+boton8.className = 'boton8 button';
+document.querySelector('.div').appendChild(boton8);
+var boton9 = document.createElement('button');
+boton9.innerHTML = 'Hello :D';
+boton9.className = 'boton9 button';
+document.querySelector('.div').appendChild(boton9);
+var boton10 = document.createElement('button');
+boton10.innerHTML = 'Hello :D';
+boton10.className = 'boton10 button';
+document.querySelector('.div').appendChild(boton10);
+var boton11 = document.createElement('button');
+boton11.innerHTML = 'Hello :D';
+boton11.className = 'boton11 button';
+document.querySelector('.div').appendChild(boton11);
+var boton12 = document.createElement('button');
+boton12.innerHTML = 'Hello :D';
+boton12.className = 'boton12 button';
+document.querySelector('.div').appendChild(boton12);
+var boton13 = document.createElement('button');
+boton13.innerHTML = 'Hello :D';
+boton13.className = 'boton13 button';
+document.querySelector('.div').appendChild(boton13);
+var boton14 = document.createElement('button');
+boton14.innerHTML = 'Hello :D';
+boton14.className = 'boton14 button';
+document.querySelector('.div').appendChild(boton14);
+var boton15 = document.createElement('button');
+boton15.innerHTML = 'Hello :D';
+boton15.className = 'boton15 button';
+document.querySelector('.div').appendChild(boton15);
+var boton16 = document.createElement('button');
+boton16.innerHTML = 'Hello :D';
+boton16.className = 'boton16 button';
+document.querySelector('.div').appendChild(boton16);
+
+
+
+boton1.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#e6f99d';
+  document.querySelector('.h1').style.color = '#041122';
+  document.querySelector('.h2',).style.color = '#041122';
+  document.querySelector('.header').style.backgroundColor = '#041122';
+  document.querySelector('.logo').style.color = '#c8e98e';
+  document.querySelector('.uil-bars').style.color = '#e6f99d';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#7fda89';
+  document.querySelector('#color1').style.backgroundColor = '#7fda89';
+  document.querySelector('#color2').style.backgroundColor = '#7fda89';
+  document.querySelector('#color3').style.backgroundColor = '#7fda89';
+  document.querySelector('#color4').style.backgroundColor = '#7fda89';
+});
+
+boton2.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#7e9991';
+  document.querySelector('.h1',).style.color = '#000000';
+  document.querySelector('.h2',).style.color = '#000000';
+  document.querySelector('.header').style.backgroundColor = '#000000';
+  document.querySelector('.logo').style.color = '#a69682';
+  document.querySelector('.uil-bars').style.color = '#d8770c';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#d8770c';
+  document.querySelector('#color1').style.backgroundColor = '#d8770c';
+  document.querySelector('#color2').style.backgroundColor = '#d8770c';
+  document.querySelector('#color3').style.backgroundColor = '#d8770c';
+  document.querySelector('#color4').style.backgroundColor = '#d8770c';
+});
+
+boton3.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#e1b7ed';
+  document.querySelector('.h1').style.color = '#e04891';
+  document.querySelector('.h2',).style.color = '#e04891';
+  document.querySelector('.header').style.backgroundColor = '#e04891';
+  document.querySelector('.logo').style.color = '#f5e1e2';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#d1e389';
+  document.querySelector('#color1').style.backgroundColor = '#d1e389';
+  document.querySelector('#color2').style.backgroundColor = '#d1e389';
+  document.querySelector('#color3').style.backgroundColor = '#d1e389';
+  document.querySelector('#color4').style.backgroundColor = '#d1e389';
+});
+
+boton4.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#8f2f45';
+  document.querySelector('.h1').style.color = '#2b0215';
+  document.querySelector('.h2',).style.color = '#2b0215';
+  document.querySelector('.header').style.backgroundColor = '#2b0215';
+  document.querySelector('.logo').style.color = '#d24d6c';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#ee2853';
+  document.querySelector('#color1').style.backgroundColor = '#ee2853';
+  document.querySelector('#color2').style.backgroundColor = '#ee2853';
+  document.querySelector('#color3').style.backgroundColor = '#ee2853';
+  document.querySelector('#color4').style.backgroundColor = '#ee2853';
+});
+
+boton5.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#96c3a6';
+  document.querySelector('.h1').style.color = '#221d34';
+  document.querySelector('.h2',).style.color = '#221d34';
+  document.querySelector('.header').style.backgroundColor = '#221d34';
+  document.querySelector('.logo').style.color = '#dae2cb';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#6cb6a5';
+  document.querySelector('#color1').style.backgroundColor = '#6cb6a5';
+  document.querySelector('#color2').style.backgroundColor = '#6cb6a5';
+  document.querySelector('#color3').style.backgroundColor = '#6cb6a5';
+  document.querySelector('#color4').style.backgroundColor = '#6cb6a5';
+});
+
+boton6.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#00f8ea';
+  document.querySelector('.h1').style.color = '#352f67';
+  document.querySelector('.h2',).style.color = '#352f67';
+  document.querySelector('.header').style.backgroundColor = '#00d6ff';
+  document.querySelector('.logo').style.color = '#352f67';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#98ffd4';
+  document.querySelector('#color1').style.backgroundColor = '#98ffd4';
+  document.querySelector('#color2').style.backgroundColor = '#98ffd4';
+  document.querySelector('#color3').style.backgroundColor = '#98ffd4';
+  document.querySelector('#color4').style.backgroundColor = '#98ffd4';
+});
+
+boton7.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#3c110c';
+  document.querySelector('.h1').style.color = '#ecca56';
+  document.querySelector('.h2',).style.color = '#ecca56';
+  document.querySelector('.header').style.backgroundColor = '#613021';
+  document.querySelector('.logo').style.color = '#ecca56';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#ecca56';
+  document.querySelector('#color1').style.backgroundColor = '#ecca56';
+  document.querySelector('#color2').style.backgroundColor = '#ecca56';
+  document.querySelector('#color3').style.backgroundColor = '#ecca56';
+  document.querySelector('#color4').style.backgroundColor = '#ecca56';
+});
+
+boton8.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#09202e';
+  document.querySelector('.h1').style.color = '#f3f3f3';
+  document.querySelector('.h2',).style.color = '#f3f3f3';
+  document.querySelector('.header').style.backgroundColor = '#2b7fc8';
+  document.querySelector('.logo').style.color = '#d6ceea';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#f3f3f3';
+  document.querySelector('#color1').style.backgroundColor = '#f3f3f3';
+  document.querySelector('#color2').style.backgroundColor = '#f3f3f3';
+  document.querySelector('#color3').style.backgroundColor = '#f3f3f3';
+  document.querySelector('#color4').style.backgroundColor = '#f3f3f3';
+});
+
+boton9.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#4d0910';
+  document.querySelector('.h1').style.color = '#b97be5'
+  document.querySelector('.h2',).style.color = '#b97be5'
+  document.querySelector('.header').style.backgroundColor = '#b97be5';
+  document.querySelector('.logo').style.color = '#f8efec';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#f8efec';
+  document.querySelector('#color1').style.backgroundColor = '#f8efec';
+  document.querySelector('#color2').style.backgroundColor = '#f8efec';
+  document.querySelector('#color3').style.backgroundColor = '#f8efec';
+  document.querySelector('#color4').style.backgroundColor = '#f8efec';
+});
+
+boton10.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#d0cd25';
+  document.querySelector('.h1').style.color = '#1f1407'
+  document.querySelector('.h2',).style.color = '#1f1407'
+  document.querySelector('.header').style.backgroundColor = '#efe76f';
+  document.querySelector('.logo').style.color = '#827870';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#f3eae8';
+  document.querySelector('#color1').style.backgroundColor = '#f3eae8';
+  document.querySelector('#color2').style.backgroundColor = '#f3eae8';
+  document.querySelector('#color3').style.backgroundColor = '#f3eae8';
+  document.querySelector('#color4').style.backgroundColor = '#f3eae8';
+});
+
+boton11.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#102d4b';
+  document.querySelector('.h1').style.color = '#0efff9'
+  document.querySelector('.h2',).style.color = '#0efff9'
+  document.querySelector('.header').style.backgroundColor = '#0971af';
+  document.querySelector('.logo').style.color = '#0efff9';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#08bcff';
+  document.querySelector('#color1').style.backgroundColor = '#08bcff';
+  document.querySelector('#color2').style.backgroundColor = '#08bcff';
+  document.querySelector('#color3').style.backgroundColor = '#08bcff';
+  document.querySelector('#color4').style.backgroundColor = '#08bcff';
+});
+
+boton12.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#63283a';
+  document.querySelector('.h1').style.color = '#f1bb74'
+  document.querySelector('.h2',).style.color = '#f1bb74'
+  document.querySelector('.header').style.backgroundColor = '#843847';
+  document.querySelector('.logo').style.color = '#ef846a';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#ef846a';
+  document.querySelector('#color1').style.backgroundColor = '#ef846a';
+  document.querySelector('#color2').style.backgroundColor = '#ef846a';
+  document.querySelector('#color3').style.backgroundColor = '#ef846a';
+  document.querySelector('#color4').style.backgroundColor = '#ef846a';
+});
+
+boton13.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#1e381c';
+  document.querySelector('.h1').style.color = '#d0d661'
+  document.querySelector('.h2',).style.color = '#d0d661'
+  document.querySelector('.header').style.backgroundColor = '#486831';
+  document.querySelector('.logo').style.color = '#121709';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#88984a';
+  document.querySelector('#color1').style.backgroundColor = '#88984a';
+  document.querySelector('#color2').style.backgroundColor = '#88984a';
+  document.querySelector('#color3').style.backgroundColor = '#88984a';
+  document.querySelector('#color4').style.backgroundColor = '#88984a';
+});
+
+boton14.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#ff0f3c';
+  document.querySelector('.h1').style.color = '#dde81c'
+  document.querySelector('.h2',).style.color = '#dde81c'
+  document.querySelector('.header').style.backgroundColor = '#ff7007';
+  document.querySelector('.logo').style.color = '#dde81c';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#9cfe6b';
+  document.querySelector('#color1').style.backgroundColor = '#9cfe6b';
+  document.querySelector('#color2').style.backgroundColor = '#9cfe6b';
+  document.querySelector('#color3').style.backgroundColor = '#9cfe6b';
+  document.querySelector('#color4').style.backgroundColor = '#9cfe6b';
+});
+
+boton15.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#ba0000';
+  document.querySelector('.h1').style.color = '#faffaf'
+  document.querySelector('.h2',).style.color = '#faffaf'
+  document.querySelector('.header').style.backgroundColor = '#c43f00';
+  document.querySelector('.logo').style.color = '#f5df6d';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#dd9500';
+  document.querySelector('#color1').style.backgroundColor = '#dd9500';
+  document.querySelector('#color2').style.backgroundColor = '#dd9500';
+  document.querySelector('#color3').style.backgroundColor = '#dd9500';
+  document.querySelector('#color4').style.backgroundColor = '#dd9500';
+});
+
+boton16.addEventListener('click', function(){
+  document.body.style.backgroundColor = '#adebff';
+  document.querySelector('.h1').style.color = '#001f35'
+  document.querySelector('.h2',).style.color = '#001f35'
+  document.querySelector('.header').style.backgroundColor = '#001f35';
+  document.querySelector('.logo').style.color = '#e2e2e2';
+  document.querySelector('.nav-bar ul li a').style.backgroundColor = '#74c5fa';
+  document.querySelector('#color1').style.backgroundColor = '#74c5fa';
+  document.querySelector('#color2').style.backgroundColor = '#74c5fa';
+  document.querySelector('#color3').style.backgroundColor = '#74c5fa';
+  document.querySelector('#color4').style.backgroundColor = '#74c5fa';
+});
